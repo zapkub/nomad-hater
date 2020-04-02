de1686d5dc1bb705ba290f3a8372539a23cc1279
Rungsikorn R <rungsikorn.r@kbtg.tech>
2020-04-02 20:06:52 +0700

    Initial Commit

---
+++ nomad-stealing-preventor/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  nomad-stealing-preventor
+//
+//  Created by Rungsikorn Rungsikavanich on 4/2/20.
+//  Copyright © 2020 Rungsikorn Rungsikavanich. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
